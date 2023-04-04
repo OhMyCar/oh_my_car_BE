6d3dd04816a109108e5eb6fff3da7acb74d47f2c
Hennessy-on-melody <114402124+Hennessy-on-melody@users.noreply.github.com>
2023-04-04 23:13:21 +0900

    Add files via upload

---
+++ omc/src/test/java/com/hotsix/omc/OmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotsix.omc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
