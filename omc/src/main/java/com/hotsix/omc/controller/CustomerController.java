784de93e6d91b31e96b169e7cb493632d8c2c709
hennessy_on_melody <attempt3699@gmail.com>
2023-04-06 12:36:58 +0900

    Feat#003, 004: Customer Signup, Jwt, Swagger Config, Spring Security Config(수정) --- Customer Signup: 회원가입 부분 구현했는데 휘성님과 겹칠 것 같아서 seller는 아직 작성X -> (수정) + 로그인 구현: 로그인 할 때 payloads 생성되며 validateEmailAndPassword 로 계정 검증 -> 계정이 없는 경우 Exception -> 컨트롤러에 로그인 구현, @Controller -> @RestController 변경 -> CustomerLoginForm 작성: 통일성있게 CustomerSigninForm 으로 작성하려 했으나 밑줄그어지는거 보기 싫어서 CustomerLoginForm 으로 지었습니다.
    -> 휘성님 필독!!! : 모든 SignupForm email에 @Email 어노테이션 달아서 이메일은 형식 지정해주었습니다. 휴대폰 번호만 정규식지정하심 될 것 같습니다!!
    
    (수정)ErrorCode, UsersException 작성
    
    Jwt: Token Provider, Filter, Entry Point, Exception Filter 작성
    ps.혹시 jwt secret 키 임의로 생성하는법 아시면 알려주세요(글자수대로 임의로 입력헀답니다..)
    
    Swagger Config: swagger configuration 작성했습니다. 더하여 Spring Security 설정보시면
    anyRequest 가 작동하지 않아서 일단 antMatcher 로 customer 와 seller 권한에 role 지정해놨습니다.
    
    Spring Security Config: 회원로그인, 회원가입단과 스웨거는 permitAll 해놨읍니다.
    
    기타:
    - gradle 에 swagger 추가하였습니다.
    - 패키지 몇개 만들어놨습니다.
    - yml 파일 spring.mvc ~ 설정 추가되었습니다.
    
    확인해보시고 리뷰 부탁드립니다.

--- omc/src/main/java/com/hotsix/omc/controller/CustomerController.java
+++ omc/src/main/java/com/hotsix/omc/controller/CustomerController.java
@@ -1,18 +1,20 @@
 package com.hotsix.omc.controller;
 
+import com.hotsix.omc.domain.form.customer.CustomerLoginForm;
 import com.hotsix.omc.domain.form.customer.CustomerSignupForm.Request;
 import com.hotsix.omc.domain.form.customer.CustomerSignupForm.Response;
+import com.hotsix.omc.domain.form.token.TokenInfo;
 import com.hotsix.omc.service.CustomerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/customer")
 public class CustomerController {
@@ -21,5 +23,12 @@ public class CustomerController {
     public ResponseEntity<Response> registerMember(
             @RequestBody @Valid Request request) {
         return ResponseEntity.ok(customerService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenInfo> login(
+            @RequestBody @Valid CustomerLoginForm form){
+        TokenInfo customerToken = customerService.login(form.getEmail(), form.getPassword());
+        return ResponseEntity.ok(customerToken);
     }
 }
--- omc/src/main/java/com/hotsix/omc/service/CustomerServiceImpl.java
+++ omc/src/main/java/com/hotsix/omc/service/CustomerServiceImpl.java
@@ -2,32 +2,51 @@ package com.hotsix.omc.service;
 
 import com.hotsix.omc.components.MailComponents;
 import com.hotsix.omc.domain.entity.Customer;
+import com.hotsix.omc.domain.entity.Seller;
 import com.hotsix.omc.domain.form.customer.CustomerSignupForm;
 import com.hotsix.omc.domain.form.customer.CustomerSignupForm.Response;
+import com.hotsix.omc.domain.form.token.TokenInfo;
+import com.hotsix.omc.exception.UsersException;
+import com.hotsix.omc.jwt.JwtTokenProvider;
 import com.hotsix.omc.repository.CustomerRepository;
+import com.hotsix.omc.repository.SellerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static com.hotsix.omc.domain.entity.CustomerStatus.UNAUTHORIZED;
+import static com.hotsix.omc.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService, UserDetailsService {
     private final CustomerRepository customerRepository;
     private final MailComponents mailComponents;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SellerRepository sellerRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider tokenProvider;
 
     @Override
     public Response register(CustomerSignupForm.Request request) {
         Optional<Customer> optionalCustomer = customerRepository.findByEmail(request.getEmail());
         if (optionalCustomer.isPresent()){
-            throw new UsernameNotFoundException("ALREADY_EXIST_USER");
+            throw new UsersException(ALREADY_EXIST_USER);
         }
         String encPw = BCrypt.hashpw(request.getPassword(), BCrypt.gensalt());
         String uuid = UUID.randomUUID().toString();
@@ -52,8 +71,54 @@ public class CustomerServiceImpl implements CustomerService, UserDetailsService 
         return Response.from(request);
     }
 
+    public TokenInfo login(String email, String password){
+        // id, pw 기반 Authentication 객체생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(email, password);
+
+        // 검증 (비밀번호 확인)
+        // authenticate 실행 될 때 loadByUserByUsername 실행
+        validateEmailAndPassword(email, password);
+        Authentication authentication = authenticationManagerBuilder
+                .getObject()
+                .authenticate(authenticationToken);
+
+        return tokenProvider.generateToken(authentication);
+    }
+
+    private void validateEmailAndPassword(String email, String password) {
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new UsersException(EMAIL_NOT_EXIST));
+
+        if (!passwordEncoder.matches(password, customer.getPassword())){
+            throw new UsersException(PASSWORD_NOT_MATCH);
+        }
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) {
+        if (customerRepository.findByEmail(email).isPresent()){
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("EMAiL NOT FOUND -> " + email));
+            List<GrantedAuthority> customerGrantedAuthorities = new ArrayList<>();
+            customerGrantedAuthorities.add(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
+
+            return new User(customer.getEmail(),
+                    customer.getPassword(),
+                    customerGrantedAuthorities);
+        }
+
+        else if (sellerRepository.findByEmail(email).isPresent()) {
+            Seller seller = sellerRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("EMAiL NOT FOUND -> " + email));
+
+            List<GrantedAuthority> sellerGrantedAuthorities = new ArrayList<>();
+            sellerGrantedAuthorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+            return new User(seller.getEmail(),
+                    seller.getPassword(),
+                    sellerGrantedAuthorities);
+        }
         return null;
     }
 }
