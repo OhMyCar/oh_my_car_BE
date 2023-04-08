944284e37f2201e1dd0e9717a2967156f49ef951
wheesung lee <wlee9486@naver.com>
2023-04-08 15:32:20 +0900

    feat.008-UPDATE-STORE

--- omc/src/main/java/com/hotsix/omc/exception/ErrorCode.java
+++ omc/src/main/java/com/hotsix/omc/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     PASSWORD_NOT_MATCH("패스워드가 일치하지 않습니다."),
     BAD_REQUEST("잘못된 요청입니다."),
     ALREADY_EXIST_USER("이미 존재하는 회원입니다."),
-    SELLER_NOT_FOUND("셀러를 찾을 수 없습니다.");
+    SELLER_NOT_FOUND("셀러를 찾을 수 없습니다."),
+    STORE_NOT_FOUND("업체를 찾을 수 없습니다.");
     private final String errorMessage;
 
 }
--- omc/src/main/java/com/hotsix/omc/service/StoreService.java
+++
@@ -1,9 +0,0 @@
-package com.hotsix.omc.service;
-
-
-import com.hotsix.omc.domain.form.store.StoreRegisterForm;
-
-public interface StoreService {
-    void deleteStore(Long id);
-    public String registerStore(StoreRegisterForm form);
-}
