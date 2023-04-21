4722fb0757785e1b0eb60f0cf24f1441bcf56bd2
hennessy_on_melody <attempt3699@gmail.com>
2023-04-21 18:29:23 +0900

    refactor#51 : conflict 수정

--- omc/src/main/java/com/hotsix/omc/exception/ErrorCode.java
+++ omc/src/main/java/com/hotsix/omc/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     BAD_REQUEST("잘못된 요청입니다."),
     ALREADY_EXIST_USER("이미 존재하는 회원입니다."),
     SELLER_NOT_FOUND("셀러를 찾을 수 없습니다."),
+    STORE_NOT_FOUND("업체를 찾을 수 없습니다."),
     FAILED_FIREBASE_INIT("failed firebase initialize. "),
     NOTIFICATION_NOT_FOUND("notification not found."),
     NOTIFICATION_DELETED("notification deleted."),
@@ -19,8 +20,7 @@ public enum ErrorCode {
     CAN_NOT_FOUND_TOKEN("token not found."),
     NOT_FOUNT_NOTIFICATION("not found notification."),
     NOTIFICATION_RECEIVER_NOT_MATCH("notification receiver doesn't match."),
-    UNMATCH_NOTIFICATION_RECEIVER("unmatch notification receiver."),
-    STORE_NOT_FOUND("업체를 찾을 수 없습니다.");
+    UNMATCH_NOTIFICATION_RECEIVER("unmatch notification receiver.");
     private final String errorMessage;
 
 }
