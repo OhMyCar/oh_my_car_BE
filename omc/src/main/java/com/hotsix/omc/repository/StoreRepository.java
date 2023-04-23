385b4800890d464e02cc80e82e459128dfeb2c7c
wheesunglee <wlee9486@naver.com>
2023-04-23 16:23:12 +0900

    054-SEARCH-BY-LOCATION

--- omc/src/main/java/com/hotsix/omc/repository/StoreRepository.java
+++ omc/src/main/java/com/hotsix/omc/repository/StoreRepository.java
@@ -1,9 +1,10 @@
 package com.hotsix.omc.repository;
 
-import com.hotsix.omc.domain.entity.Address;
 import com.hotsix.omc.domain.entity.Seller;
 import com.hotsix.omc.domain.entity.Store;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,15 @@ import java.util.Optional;
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
     Optional<Store> findByIdAndSellerId(Long storeId, Long sellerId);
+
     List<Store> findBySellerId(Long sellerId);
+
     Optional<Store> findByAddress_Zipcode(String zipcode);
+
     Optional<Store> findBySellerAndName(Seller seller, String name);
+
+    @Query(nativeQuery = true, value = "select *, ST_Distance_Sphere(Point(store.longitude, store.latitude), Point(:currentLng, :currentLat))/1000 as distance from store order by distance limit 5")
+    List<Store> findNearestStores(@Param("currentLng") double currentLng, @Param("currentLat") double currentLat);
+
+
 }
